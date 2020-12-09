0551fa2a66a9f9e906059d91929aa4c5d4aa52ad
duxl <645575165@qq.com>
2020-12-09 17:55:00 +0800

    分类

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,18 +1,55 @@
 // pages/category/index.js
+import{ request } from "../../request/index.js"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 左边菜单数据
+    leftMenuList:[],
+    // 右边商品数据
+    rightContent:[],
+    // 被点击的左侧菜单索引
+    curentIndex: 0
   },
+  //接口的返回数据
+  Cates:[],
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getCates();
+  },
 
+  // 获取分类数据
+  getCates: function() {
+    request({url: "https://api-hmugo-web.itheima.net/api/public/v1/categories"})
+    .then(res=>{
+      //console.log(res)
+      this.Cates = res.data.message;
+
+      // 构造左侧的大菜单数据
+      let leftMenuList = this.Cates.map(v=>v.cat_name)
+      // 构造右侧的商品数据
+      let rightContent = this.Cates[0].children
+      this.setData({
+        leftMenuList,
+        rightContent
+      })
+    })
+  },
+
+  handleItemTap: function(e) {
+    console.log(e)
+    let curentIndex = e.target.dataset.index;
+    let rightContent = this.Cates[curentIndex].children;
+    this.setData({
+      curentIndex,
+      rightContent
+    })
   },
 
   /**
@@ -26,7 +63,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
   },
 
   /**
