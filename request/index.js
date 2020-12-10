b768708ef563fdf941029bddec744dabff4ff77f
duxl <645575165@qq.com>
2020-12-10 16:20:20 +0800

    全局添加 接口加载中 效果

--- request/index.js
+++ request/index.js
@@ -1,4 +1,10 @@
 export const request=(params)=>{
+    // 显示加载中
+    wx.showLoading({
+        title: "加载中",
+        mask: true // 蒙板
+    });
+    
     // 定义公共的url
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
     return new Promise((resolve, reject)=>{
@@ -10,6 +16,9 @@ export const request=(params)=>{
             },
             fail: (err) => {
                 reject(err);
+            },
+            complete: () => {
+                wx.hideLoading();
             }
         });          
     })
