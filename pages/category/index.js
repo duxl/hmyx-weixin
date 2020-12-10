40ae394caa792376c5ec9c6c75fec85bbb392a2f
duxl <645575165@qq.com>
2020-12-10 10:41:08 +0800

    分类加缓存

--- pages/category/index.js
+++ pages/category/index.js
@@ -21,7 +21,35 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCates();
+    /*
+    1 现判断本地存储的有没有旧数据
+    2 没有旧数据 直接发送请求
+    3 有旧数据 同时旧数据没有过期就是用
+    */
+
+    // 1 获取本地存储中的数据
+    const Cates = wx.getStorageSync('cates');
+    // 2 判断
+    if(!Cates) {
+      // 不存在，发送请求获取数据
+      this.getCates();
+    } else {
+      // 有数据，定义过期时间 10s 改成 5分钟
+      if(Date.now() - Cates.time > 1000 * 10) {
+        // 重新发送请求
+        this.getCates();
+      } else {
+        // 可以使用旧数据
+        console.log("使用旧数据")
+        this.Cates = Cates.data;
+        let leftMenuList = this.Cates.map(v=>v.cat_name)
+        let rightContent = this.Cates[0].children
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      }
+    }
   },
 
   // 获取分类数据
@@ -30,6 +58,13 @@ Page({
     .then(res=>{
       //console.log(res)
       this.Cates = res.data.message;
+
+      // 把接口返回的数据存入本地缓存中
+      wx.setStorageSync("cates", {
+        time: Date.now(),
+        data: this.Cates
+      });
+        
 
       // 构造左侧的大菜单数据
       let leftMenuList = this.Cates.map(v=>v.cat_name)
